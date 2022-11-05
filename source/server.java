2184a6b6b58935c45e7d05c48f7c8336bf4081ef
Damian Brzezinski <244001@edu.p.lodz.pl>
2022-11-05 12:29:02 +0100

    timer

--- source/server.java
+++ source/server.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -19,6 +21,9 @@ public class Test {
         @Override
         public void handle(HttpExchange t) throws IOException {
             String response = "Hello World from java!\n";
+            LocalDateTime myDateObj = LocalDateTime.now();
+            DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("HH:mm:ss");
+            response+=myDateObj.format(myFormatObj)+"\n";
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());

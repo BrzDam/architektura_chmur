645654af92dd46e316be39e0eec57028a7f381fa
Damian Brzezinski <244001@edu.p.lodz.pl>
2022-11-19 12:18:37 +0100

    time

--- lab2/source/server.java
+++ lab2/source/server.java
@@ -3,6 +3,8 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.HashMap;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -20,17 +22,40 @@ public class Test {
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response = "Hello World from java!\n";
-            LocalDateTime myDateObj = LocalDateTime.now();
-            DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("HH:mm:ss");
-            response+=myDateObj.format(myFormatObj)+"\n";
-            response+=t.getHttpContext().getAttribute("test");
+            String quary = t.getRequestURI().getQuery();
+            Map<String,String> queryMap = queryToMap(quary);
+            
+            String response = "";
+            if(queryMap == null) {
+            	response += "Hello World from java!\n";
+            } else if (queryMap.get("cmd").equals("time")) {
+            	LocalDateTime myDateObj = LocalDateTime.now();
+		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("HH:mm:ss");
+		response+=myDateObj.format(myFormatObj)+"\n";
+            }
+            
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
 	    System.out.println("Served hello world...");
         }
+    }
+    
+    static public Map<String, String> queryToMap(String query) {
+	    if(query == null) {
+		return null;
+	    }
+	    Map<String, String> result = new HashMap<>();
+	    for (String param : query.split("&")) {
+		String[] entry = param.split("=");
+		if (entry.length > 1) {
+		    result.put(entry[0], entry[1]);
+		}else{
+		    result.put(entry[0], "");
+		}
+	    }
+	    return result;
     }
 
 }

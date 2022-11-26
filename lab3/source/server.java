ec44a9ed12bbf486515fbb1ec3c7d59d4c88bb38
student <student@L407-5.helium.com>
2022-11-26 10:21:27 +0100

    init commit

---
+++ lab3/source/server.java
@@ -0,0 +1,69 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(4080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String quary = t.getRequestURI().getQuery();
+            Map<String,String> queryMap = queryToMap(quary);
+            
+            String response = "";
+            if(queryMap == null) {
+            	response += "Hello World from java!\n";
+            } else if (queryMap.get("cmd").equals("time")) {
+            	LocalDateTime myDateObj = LocalDateTime.now();
+		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("HH:mm:ss");
+		response+=myDateObj.format(myFormatObj)+"\n";
+            } else if (queryMap.get("cmd").equals("rev")) {
+            	String str = queryMap.get("str");
+            	String nstr = "";
+            	for (int i=0; i<str.length(); i++) {
+			char ch= str.charAt(i);
+			nstr= ch+nstr;
+		}
+		response += nstr;
+            }
+            
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+	    System.out.println("Served hello world...");
+        }
+    }
+    
+    static public Map<String, String> queryToMap(String query) {
+	    if(query == null) {
+		return null;
+	    }
+	    Map<String, String> result = new HashMap<>();
+	    for (String param : query.split("&")) {
+		String[] entry = param.split("=");
+		if (entry.length > 1) {
+		    result.put(entry[0], entry[1]);
+		}else{
+		    result.put(entry[0], "");
+		}
+	    }
+	    return result;
+    }
+
+}
